c18061f3e9a1f2be8650c0cee1b7e047df3abdd9
Radu Potop <radu@wooptoo.com>
2021-03-31 13:16:28 +0100

    Initial commit

---
+++ Tail/Football/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+class BaseModel(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Stadium(BaseModel):
+    name = models.CharField(max_length=64, unique=True)
+    capacity = models.PositiveIntegerField()
+
+
+class League(BaseModel):
+    name = models.CharField(max_length=64, unique=True)
+
+
+class FootballTeam(BaseModel):
+    name = models.CharField(max_length=64, unique=True)
+    country = models.CharField(max_length=2, db_index=True)
+    year_founded = models.DateField()
+    stadium = models.ForeignKey(Stadium, on_delete=models.CASCADE, blank=True, null=True)
+    league = models.ForeignKey(League, on_delete=models.CASCADE, blank=True, null=True)
