ac71b8206ca7102ca8809a9ae6cc86dcddba6f30
Radu Potop <radu@wooptoo.com>
2021-04-06 00:47:31 +0100

    Test fixes

--- Football/tests/tests.py
+++ Football/tests/tests.py
@@ -4,6 +4,8 @@ from Football.tests.factories import SuperUserFactory
 
 
 class FootballApiTest(APITestCase):
+    fixtures = ['fixtures/initial_data.yaml']
+
     def setUp(self):
         su = SuperUserFactory()
         self.client = APIClient()
@@ -11,8 +13,8 @@ class FootballApiTest(APITestCase):
 
     def test_post_team(self):
         postdata = dict(
-            name='Arsenal FC',
-            country='GB',
+            name='FCSB',
+            country='RO',
             year_founded='1961-01-01',
         )
         response = self.client.post('/api/v1/teams/', postdata, format='json')
@@ -20,9 +22,13 @@ class FootballApiTest(APITestCase):
         assert response.status_code == 201
         assert postdata['name'] == response.json()['name']
 
-    # def test_get_team_by_country(self):
-    #     response = self.client.get('/api/v1/teams/?country=GB')
+    def test_get_team_by_country(self):
+        response = self.client.get('/api/v1/teams/?country=GB')
 
-    #     assert response.status_code == 200
-    #     print(response.json())
-    #     assert response.json()['name'] == 'Arsenal FC'
+        assert response.status_code == 200
+        assert len(response.json()) == 4
+
+    def test_get_team_by_name(self):
+        response = self.client.get('/api/v1/teams/?name=Manchester%20United')
+
+        assert response.status_code == 200
